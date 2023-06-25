6e8cdfcf564e05080d1836ece44cd831c315a3c3
msaepul <muhamadsaepul12@gmail.com>
2023-06-25 10:43:42 +0700

    add new view

--- resources/views/Masterdata/sparepart/addsparepart.blade copy.php
+++ resources/views/Masterdata/sparepart/addsparepart.blade copy.php
@@ -1,148 +1,73 @@
-@extends('layouts.mainlayout')
+<!DOCTYPE html>
+<html>
 
-@section('title', 'Add-sparepart')
+<head>
+    <title>Dynamic Multiple Input dengan Select2</title>
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css">
+</head>
 
+<body>
+    <h2>Dynamic Multiple Input dengan Select2</h2>
 
-@section('content')
-    <!-- Content Wrapper. Contains page content -->
-    <div class="content-wrapper">
-        <!-- Content Header (Page header) -->
-        <section class="content-header">
-            <div class="container-fluid">
-                <div class="row mb-2">
-                    <div class="col-sm-6">
-                        <h1>Tambah Sparepart</h1>
-                    </div>
-                    <div class="col-sm-6">
-                        <ol class="breadcrumb float-sm-right">
-                            <li class="breadcrumb-item"><a href="#">Home</a></li>
-                            <li class="breadcrumb-item active">Contact us</li>
-                        </ol>
-                    </div>
-                </div>
-            </div><!-- /.container-fluid -->
-        </section>
+    <table id="items_table">
+        <tr>
+            <th>Nama Barang</th>
+            <th>Qty</th>
+            <th>Harga</th>
+            <th>Aksi</th>
+        </tr>
+        <tr>
+            <td>
+                <select class="item-select" name="item[]">
+                    <option value="Barang 1">Barang 1</option>
+                    <option value="Barang 2">Barang 2</option>
+                    <option value="Barang 3">Barang 3</option>
+                </select>
+            </td>
+            <td><input type="number" name="qty[]" min="1"></td>
+            <td><input type="number" name="harga[]" min="0"></td>
+            <td><button onclick="deleteRow(this)">Hapus</button></td>
+        </tr>
+    </table>
 
-        <!-- Main content -->
-        <section class="content center">
-            <form action="{{ route('sparepart_proses') }}" method="POST">
-                @csrf
-                <div class="d-flex justify-content-center">
-                    <!-- Default box -->
-                    <div class="card card-primary card-outline col-12 col-md-8">
-                        <div class="card-body">
-                            @if ($errors->any())
-                                <div id="myAlert" class="alert alert-danger alert-dismissible fade show" role="alert">
-                                    <strong>Error!</strong> Terdapat beberapa masalah dalam pengisian formulir:
-                                    <ul>
-                                        @foreach ($errors->all() as $error)
-                                            <li>{{ $error }}</li>
-                                        @endforeach
-                                    </ul>
+    <button onclick="addRow()">Tambah Baris</button>
 
-                                </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            // Inisialisasi Select2 pada elemen dengan class "item-select"
+            $('.item-select').select2();
+        });
 
+        function addRow() {
+            // Mendapatkan jumlah baris saat ini di tabel
+            var rowCount = document.getElementById("items_table").rows.length;
 
-                            @endif
+            // Membuat elemen-elemen input baru
+            var newRow = document.getElementById("items_table").insertRow(rowCount);
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            var cell3 = newRow.insertCell(2);
+            var cell4 = newRow.insertCell(3);
 
+            // Mengatur HTML untuk elemen input baru
+            cell1.innerHTML =
+                '<select class="item-select" name="item[]"><option value="Barang 1">Barang 1</option><option value="Barang 2">Barang 2</option><option value="Barang 3">Barang 3</option></select>';
+            cell2.innerHTML = '<input type="number" name="qty[]" min="1">';
+            cell3.innerHTML = '<input type="number" name="harga[]" min="0">';
+            cell4.innerHTML = '<button onclick="deleteRow(this)">Hapus</button>';
 
-                            <div class="form-group row pb-2">
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="nama_sparepart" class="text-end">Nama sparepart</label>
-                                </div>
-                                <div class="col-12 col-md-7 col-sm-7">
-                                    <input type="text" id="nama_sparepart" class="form-control" name="nama_sparepart"
-                                        placeholder="Masukkan Nama Sparepart" value="{{ old('nama_sparepart') }}" />
-                                </div>
-                            </div>
+            // Menginisialisasi Select2 pada elemen select yang baru dibuat
+            $('.item-select').select2();
+        }
 
+        function deleteRow(row) {
+            var i = row.parentNode.parentNode.rowIndex;
+            document.getElementById("items_table").deleteRow(i);
+        }
+    </script>
+</body>
 
-                            <div class="form-group row pb-2 ">
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="tgl_pbl" class="text-end">Tanggal Pembelian</label>
-                                </div>
-                                <div class="col-12 col-sm-7">
-                                    <input type="date" class="form-control w-50" id="tgl_pbl" name="tgl_pbl"
-                                        value="{{ old('tgl_pbl') }}">
-                                </div>
-                            </div>
-                            <div class="form-group row pb-2">
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="nopo" class="text-end">No PO</label>
-                                </div>
-                                <div class="col-12 col-sm-7">
-                                    <input type="text" id="nopo" class="form-control" name="nopo"
-                                        placeholder="Nomor PO" />
-                                </div>
-                            </div>
-                            <div class="form-group row pb-2 ">
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="supplier" class="text-end">Supplier </label>
-                                </div>
-                                <div class="col-12 col-sm-7">
-                                    <input type="text" id="supplier" class="form-control" name="supplier"
-                                        placeholder="Supplier" value="{{ old('supplier') }}" />
-                                </div>
-                            </div>
-                            <input type="hidden" class="form-control w-50" id="id_cabang" name="id_cabang"
-                                value="{{ $cabang }}">
-                            <div class="form-group row pb-2">
-
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="ip" class="text-end">Qty</label>
-                                </div>
-                                <div class="col-12 col-sm-7">
-                                    <div class="input-group w-50">
-                                        <input type="numeric" class="form-control" placeholder="QTY" id="qty"
-                                            name="stok">
-                                    </div>
-
-                                </div>
-                            </div>
-                            <div class="form-group row pb-2">
-
-                                <div
-                                    class="col-12 col-md-3 col-sm-3 d-flex align-items-center justify-content-sm-end justify-content-lg-end">
-                                    <label for="ip" class="text-end">Harga</label>
-                                </div>
-                                <div class="col-12 col-sm-7">
-                                    <div class="input-group w-50">
-                                        <div class="input-group-prepend">
-                                            <span class="input-group-text">Rp</span>
-                                        </div>
-                                        <input type="text" class="form-control" placeholder="harga" id="harga"
-                                            name="harga">
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="card-footer">
-                                    <input type="button" class="btn btn-secondary float-start" value="Cancel"
-                                        onclick="window.history.back();">
-
-                                    <input type="submit" class="btn btn-primary float-end" value="Tambah">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </form>
-            {{-- @include('Masterdata.modal.modaladdbrand') --}}
-    </div>
-    {{-- @include('Masterdata.modal.modaladdtype') --}}
-
-    </section>
-    <!-- /.content -->
-    </div>
-    <!-- /.content-wrapper -->
-
-    <!-- /.content -->
-
-@endsection
+</html>
--- resources/views/Masterdata/sparepart/addsparepart.blade.php
+++ resources/views/Masterdata/sparepart/addsparepart.blade.php
@@ -117,7 +117,7 @@
                                             <option value="">Pilih Sparepart</option>
                                             @foreach ($sparepart as $part)
                                                 <option value="{{ $part->id }}">
-                                                   {{ $part->nama_sparepart }}
+                                                    {{ $part->nama_sparepart }}
                                                 </option>
                                             @endforeach
                                         </select>
@@ -160,16 +160,79 @@
                                 <input type="submit" class="btn btn-primary float-end" value="Tambah">
                             </div>
             </form>
+
     </div>
     </div>
-   
+
     </div>
     <!-- /.col -->
     @include('Masterdata.modal.modaladdsupplier')
     @include('Masterdata.modal.modaladdsparepart')
     </div>
     {{-- @include('Masterdata.modal.modaladdtype') --}}
+    <div class="card">
+        <div class="card-body">
 
+            <table id="items_table">
+                <tr>
+                    <th>Nama Barang</th>
+                    <th>Qty</th>
+                    <th>Harga</th>
+                    <th>Aksi</th>
+                </tr>
+                <tr>
+                    <td>
+                        <select class="item-select" name="item[]">
+                            <option value="Barang 1">Barang 1</option>
+                            <option value="Barang 2">Barang 2</option>
+                            <option value="Barang 3">Barang 3</option>
+                        </select>
+                    </td>
+                    <td><input type="number" name="qty[]" min="1"></td>
+                    <td><input type="number" name="harga[]" min="0"></td>
+                    <td><button onclick="deleteRow(this)">Hapus</button></td>
+                </tr>
+            </table>
+
+            <button onclick="addRow()">Tambah Baris</button>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+            <script>
+                $(document).ready(function() {
+                    // Inisialisasi Select2 pada elemen dengan class "item-select"
+                    $('.item-select').select2();
+                });
+
+                function addRow() {
+                    // Mendapatkan jumlah baris saat ini di tabel
+                    var rowCount = document.getElementById("items_table").rows.length;
+
+                    // Membuat elemen-elemen input baru
+                    var newRow = document.getElementById("items_table").insertRow(rowCount);
+                    var cell1 = newRow.insertCell(0);
+                    var cell2 = newRow.insertCell(1);
+                    var cell3 = newRow.insertCell(2);
+                    var cell4 = newRow.insertCell(3);
+
+                    // Mengatur HTML untuk elemen input baru
+                    cell1.innerHTML =
+                        '<select class="item-select" name="item[]"><option value="Barang 1">Barang 1</option><option value="Barang 2">Barang 2</option><option value="Barang 3">Barang 3</option></select>';
+                    cell2.innerHTML = '<input type="number" name="qty[]" min="1">';
+                    cell3.innerHTML = '<input type="number" name="harga[]" min="0">';
+                    cell4.innerHTML = '<button onclick="deleteRow(this)">Hapus</button>';
+
+                    // Menginisialisasi Select2 pada elemen select yang baru dibuat
+                    $('.item-select').select2();
+                }
+
+                function deleteRow(row) {
+                    var i = row.parentNode.parentNode.rowIndex;
+                    document.getElementById("items_table").deleteRow(i);
+                }
+            </script>
+        </div>
+    </div>
     </section>
 
     <!-- /.content -->
