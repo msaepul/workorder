40b5a89a2551ce4d7eef9ebfe608b8a1ebe752a6
msaepul12 <muhamadsaepul12@gmail.com>
2023-06-26 18:59:01 +0700

    add new input

--- resources/views/Admin/profile.blade.php
+++ resources/views/Admin/profile.blade.php
@@ -53,9 +53,55 @@
                                     aria-labelledby="tab1-tab">
                                     <div class="card">
                                         <div class="card-body">
-                                            <h5 class="card-title">Tab 1</h5>
-                                            <p class="card-text">Ini adalah isi dari Tab 1.</p>
+                                            <p class="card-text">
+                                            <div class="row g-0">
+                                                <div class="col-md-4 gradient-custom text-center text-white"
+                                                    style="border-top-left-radius: .5rem; border-bottom-left-radius: .5rem;">
+                                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                                                        alt="Avatar" class="img-fluid my-5" style="width: 80px;" />
+                                                    <h5>Marie Horwitz</h5>
+                                                    <p>Web Designer</p>
+                                                    <a href="#!" class="btn btn-primary"><i class="far fa-edit"></i>
+                                                        Edit</a>
+                                                </div>
+                                                <div class="col-md-8">
+                                                    <div class="card-body p-4">
+                                                        <h6>Information</h6>
+                                                        <hr class="mt-0 mb-4">
+                                                        <div class="row pt-1">
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Email</h6>
+                                                                <p class="text-muted">info@example.com</p>
+                                                            </div>
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Phone</h6>
+                                                                <p class="text-muted">123 456 789</p>
+                                                            </div>
+                                                        </div>
+                                                        <h6>Projects</h6>
+                                                        <hr class="mt-0 mb-4">
+                                                        <div class="row pt-1">
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Recent</h6>
+                                                                <p class="text-muted">Lorem ipsum</p>
+                                                            </div>
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Most Viewed</h6>
+                                                                <p class="text-muted">Dolor sit amet</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="d-flex justify-content-start">
+                                                            <a href="#!"><i
+                                                                    class="fab fa-facebook-f fa-lg me-3"></i></a>
+                                                            <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
+                                                            <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </p>
                                         </div>
+
                                     </div>
                                 </div>
                                 <div class="tab-pane fade" id="tab2" role="tabpanel" aria-labelledby="tab2-tab">
@@ -69,8 +115,45 @@
                                 <div class="tab-pane fade" id="tab3" role="tabpanel" aria-labelledby="tab3-tab">
                                     <div class="card">
                                         <div class="card-body">
-                                            <h5 class="card-title">Tab 3</h5>
-                                            <p class="card-text">Ini adalah isi dari Tab 3.</p>
+                                            <p class="card-text">
+                                            <div class="row g-0">
+                                                <div class="col-md-12">
+                                                    <div class="card-body p-4">
+                                                        <h6>Information</h6>
+                                                        <hr class="mt-0 mb-4">
+                                                        <div class="row pt-1">
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Email</h6>
+                                                                <p class="text-muted">info@example.com</p>
+                                                            </div>
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Phone</h6>
+                                                                <p class="text-muted">123 456 789</p>
+                                                            </div>
+                                                        </div>
+                                                        <h6>Projects</h6>
+                                                        <hr class="mt-0 mb-4">
+                                                        <div class="row pt-1">
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Recent</h6>
+                                                                <p class="text-muted">Lorem ipsum</p>
+                                                            </div>
+                                                            <div class="col-6 mb-3">
+                                                                <h6>Most Viewed</h6>
+                                                                <p class="text-muted">Dolor sit amet</p>
+                                                            </div>
+                                                        </div>
+                                                        <div class="d-flex justify-content-start">
+                                                            <a href="#!"><i
+                                                                    class="fab fa-facebook-f fa-lg me-3"></i></a>
+                                                            <a href="#!"><i
+                                                                    class="fab fa-twitter fa-lg me-3"></i></a>
+                                                            <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
--- resources/views/layouts/mainlayout.blade.php
+++ resources/views/layouts/mainlayout.blade.php
@@ -36,7 +36,18 @@
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        .gradient-custom {
+            /* fallback for old browsers */
+            background: #f6d365;
 
+            /* Chrome 10-25, Safari 5.1-6 */
+            background: -webkit-linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1));
+
+            /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+            background: linear-gradient(to right bottom, rgba(246, 211, 101, 1), rgba(253, 160, 133, 1))
+        }
+    </style>
 
     <style>
         .circle {
